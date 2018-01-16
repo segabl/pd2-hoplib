93d3e34981af01dbf1c142bb298ff8425caab008
Sebastian Gabl <sebastian.gabl@gmx.at>
2018-01-16 14:27:42 +0100

    Fix a condition for updating damage

--- lua/copdamage.lua
+++ lua/copdamage.lua
@@ -1,14 +1,16 @@
 local _call_listeners_original = CopDamage._call_listeners
 function CopDamage:_call_listeners(damage_info, ...)
 
-  local info = HopLib.unit_info_manager:get_user_info(damage_info.attacker_unit)
-  if info and type(damage_info.damage) == "number" then
-    info:update_damage(damage_info.damage, self._dead)
+  if type(damage_info.damage) == "number" then
+    local info = HopLib.unit_info_manager:get_user_info(damage_info.attacker_unit)
+    if info then
+      info:update_damage(damage_info.damage, self._dead)
+    end
     Hooks:Call("HopLibOnUnitDamaged", self._unit, damage_info)
-  end
-  if self._dead then
-    Hooks:Call("HopLibOnUnitDied", self._unit, damage_info)
-    HopLib.unit_info_manager:clear_info(self._unit)
+    if self._dead then
+      Hooks:Call("HopLibOnUnitDied", self._unit, damage_info)
+      HopLib.unit_info_manager:clear_info(self._unit)
+    end
   end
   
   return _call_listeners_original(self, damage_info, ...)
--- lua/sentrygundamage.lua
+++ lua/sentrygundamage.lua
@@ -3,15 +3,17 @@ function SentryGunDamage:_apply_damage(damage, dmg_shield, dmg_body, is_local, a
 
   local result = _apply_damage_original(self, damage, dmg_shield, dmg_body, is_local, attacker_unit, ...)
   
-  local info = HopLib.unit_info_manager:get_user_info(attacker_unit)
   local dmg = damage == "death" and (dmg_shield and self._SHIELD_HEALTH_INIT or dmg_body and self._HEALTH_INIT) or damage
-  if info and type(dmg) == "number" then
-    info:update_damage(dmg, self._dead)
+  if type(dmg) == "number" then
+    local info = HopLib.unit_info_manager:get_user_info(attacker_unit)
+    if info then
+      info:update_damage(dmg, self._dead)
+    end
     Hooks:Call("HopLibOnUnitDamaged", self._unit, { damage = dmg, attacker_unit = attacker_unit })
-  end
-  if self._dead then
-    Hooks:Call("HopLibOnUnitDied", self._unit, { damage = dmg, attacker_unit = attacker_unit })
-    HopLib.unit_info_manager:clear_info(self._unit)
+    if self._dead then
+      Hooks:Call("HopLibOnUnitDied", self._unit, { damage = dmg, attacker_unit = attacker_unit })
+      HopLib.unit_info_manager:clear_info(self._unit)
+    end
   end
   
   return result
