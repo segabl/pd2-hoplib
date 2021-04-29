fa354a3c23e1cb7b7aede0f48a047e17d56b8a5b
Sebastian Gabl <sebastian.gabl@gmx.at>
2021-04-29 08:49:24 +0200

    Avoid handling single damage info multiple times
    Some update introduced an issue where listeners for damage were called twice.
    While it didn't cause an issue so far, better safe than sorry and check for damage infos that have already been handled.

--- lua/copdamage.lua
+++ lua/copdamage.lua
@@ -1,6 +1,6 @@
 Hooks:PreHook(CopDamage, "_call_listeners", "_call_listeners_hoplib", function (self, damage_info)
 
-	if type(damage_info.damage) == "number" then
+	if type(damage_info.damage) == "number" and not damage_info.hoplib_handled then
 		local info = HopLib:unit_info_manager():get_info(damage_info.attacker_unit)
 		if info then
 			info:update_damage(damage_info.damage, self._dead)
@@ -10,6 +10,7 @@ Hooks:PreHook(CopDamage, "_call_listeners", "_call_listeners_hoplib", function (
 			Hooks:Call("HopLibOnUnitDied", self._unit, damage_info)
 			HopLib:unit_info_manager():clear_info(self._unit)
 		end
+		damage_info.hoplib_handled = true
 	end
 
 end)
