6dd9201797c7d2e0e9854f00e1680394acec47f5
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-01-02 21:58:30 +0100

    Improve damage reporting for sentry guns and SWAT turrets

--- lua/sentrygundamage.lua
+++ lua/sentrygundamage.lua
@@ -1,20 +1,28 @@
-Hooks:PostHook(SentryGunDamage, "_apply_damage", "_apply_damage_hoplib", function (self, damage, dmg_shield, dmg_body, is_local, attacker_unit)
+local _apply_damage_original = SentryGunDamage._apply_damage
+function SentryGunDamage:_apply_damage(damage, dmg_shield, dmg_body, is_local, attacker_unit, ...)
+	local health = self._health + self._shield_health
 
-	local dmg = damage == "death" and (dmg_shield and self._SHIELD_HEALTH_INIT or dmg_body and self._HEALTH_INIT) or damage
-	if type(dmg) == "number" then
+	local result = _apply_damage_original(self, damage, dmg_shield, dmg_body, is_local, attacker_unit, ...)
+
+	local dmg = math.max(0, health - self._health - self._shield_health)
+	if dmg > 0 then
 		local info = HopLib:unit_info_manager():get_info(attacker_unit)
 		if info then
 			info:update_damage(dmg, self._dead)
 		end
+
 		local attack_data = {
 			damage = dmg,
-			attacker_unit = attacker_unit
+			attacker_unit = attacker_unit,
+			is_shield = dmg_shield
 		}
 		Hooks:Call("HopLibOnUnitDamaged", self._unit, attack_data)
+
 		if self._dead then
 			Hooks:Call("HopLibOnUnitDied", self._unit, attack_data)
 			HopLib:unit_info_manager():clear_info(self._unit)
 		end
 	end
 
-end)
+	return result
+end
