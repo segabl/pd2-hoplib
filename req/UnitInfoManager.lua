00f4f8afd52ed6c11f6b1cce006896e8edcb253e
Sebastian Gabl <sebastian.gabl@gmx.at>
2026-05-10 11:34:21 +0200

    Add a safety check for Keepers compatibility

--- req/UnitInfoManager.lua
+++ req/UnitInfoManager.lua
@@ -37,7 +37,10 @@ function UnitInfo:init(unit, u_key, manager)
 		elseif self._owner or gstate._police[u_key] and gstate._police[u_key].is_converted or gstate:is_enemy_converted_to_criminal(unit) then
 			self._type = "joker"
 			self._name = HopLib:name_provider():name_by_unit(unit) or HopLib:name_provider():name_by_id(u_base._tweak_table)
-			self._nickname = u_base.joker_name or u_base.kpr_minion_owner_peer_id and Keepers and Keepers.get_joker_name_by_peer(Keepers, u_base.kpr_minion_owner_peer_id)
+			self._nickname = u_base.joker_name
+			if not self._nickname and u_base.kpr_minion_owner_peer_id and Keepers and Keepers.get_joker_name_by_peer then
+				self._nickname = Keepers:get_joker_name_by_peer(u_base.kpr_minion_owner_peer_id)
+			end
 			if not self._nickname or self._nickname == "" then
 				self._nickname = self._owner and managers.localization:text("hoplib_owners_unit", { OWNER = self._owner:nickname(), UNIT = self._name })
 			end
